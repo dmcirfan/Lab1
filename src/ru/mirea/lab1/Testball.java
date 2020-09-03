66080be39588d2c25bad5c1e014cfae11aba3b56
dmcirfan <danil.milshin.07.12@gmail.com>
2020-09-03 19:13:09 +0300

    tets

---
+++ src/ru/mirea/lab1/Testball.java
@@ -0,0 +1,12 @@
+package ru.mirea.lab1;
+import java.lang.*;
+public class Testball {
+    public static void main(String[] args) {
+        ball ba1 = new ball("red", 70, 410);
+        ball ba2 = new ball("blue", 75, 420);
+        ball ba3 = new ball("pink", 80, 450);
+        System.out.println(ba1);
+        System.out.println(ba2);
+        System.out.println(ba3);
+    }
+}
---
+++ src/ru/mirea/lab1/Testbook.java
@@ -0,0 +1,12 @@
+package ru.mirea.lab1;
+import java.lang.*;
+public class Testbook {
+    public static void main(String[] args){
+        book b1 = new book("The count of Monte Cristo",1300);
+        book b2 = new book("Taras Bulba",115);
+        book b3 = new book("dead Souls",470);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+    }
+}
